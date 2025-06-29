d2c7459ad85e115361dc646bf4767fdf2618ed84
Isha Bisht <isha.bisht.2781@gmail.com>
2025-06-29 19:37:36 +0530

    added files

---
+++ leftrotateinplace.java
@@ -0,0 +1,42 @@
+public class leftrotateinplace {
+  public static void main(String[] args) {
+    int a [] = { 3,2,2,5,4,1,2};
+    int k=3;int j=2;
+    int i=0;
+      while(i<j){
+      int temp = a[j];
+      a[j]=a[i];
+      a[i]=temp;
+      j--;
+      i++;
+    
+  }
+
+int l =a.length-1;
+  while (k<l) {
+    int temp = a[l];
+      a[l]=a[k];
+      a[k]=temp;
+      l--;
+      k++;
+  }
+
+
+   int m=0;
+   int n=a.length-1;
+  while(m<l){
+    int temp = a[n];
+      a[n]=a[m];
+      a[m]=temp;
+      n--;
+      m++;
+  }
+
+
+  for(int p=0;p<a.length;p++){
+    System.out.print(a[p]+" ");
+  }
+  }
+
+ 
+}
