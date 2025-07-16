a1318b3f3339bfd8cdeab43c44d1ac4a774575fd
Isha Bisht <isha.bisht.2781@gmail.com>
2025-07-16 23:09:17 +0530

    new

--- longSubstr.java
+++ longSubstr.java
@@ -4,14 +4,21 @@ public class longSubstr {
   public static void main(String[] args) {
     String s = "abcccdeba";
     HashSet<Character> h = new HashSet<>();
-    int l=0; int max=0;
+    int l=0; int m=0; int k=0;
     char c[] = s.toCharArray();
     for(int r=0;r<c.length;r++){
       while(h.contains(c[r])){
         h.remove(c[l++]);
       }
       h.add(c[r]);
+      m=Math.max(m, r-l+1);
+      k=r+1;
     }
+    System.out.println(m);
+    for(int i=l;i<k;i++){
+      System.out.print(c[i]);
+    }
+    
 
 
   }
